3b4fdc9d3cfa32efb609917ec6540d77c309b049
FUSUSU <nguyenducthuan.cit@gmail.com>
2017-01-01 10:45:47 +0700

    Change

--- app/src/main/java/com/example/teo/a1androidhidetitlebarexample/DemoActivityGithub.java
+++ app/src/main/java/com/example/teo/a1androidhidetitlebarexample/DemoActivityGithub.java
@@ -9,5 +9,8 @@ public class DemoActivityGithub extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo_github);
+
+
+        for (int i=1; i<10; i++);
     }
 }

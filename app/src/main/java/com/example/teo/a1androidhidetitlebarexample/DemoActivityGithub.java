4e86e725bca279f3e0f4c402e9ef808d479a971b
FUSUSU <nguyenducthuan.cit@gmail.com>
2017-01-01 10:41:41 +0700

    Git Demo Activity

---
+++ app/src/main/java/com/example/teo/a1androidhidetitlebarexample/DemoActivityGithub.java
@@ -0,0 +1,13 @@
+package com.example.teo.a1androidhidetitlebarexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DemoActivityGithub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_github);
+    }
+}
